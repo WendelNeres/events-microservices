34986158c58979b41edba398dc1a58c8ee8a2ea5
WendelNeres <wendelneres.dev@gmail.com>
2025-05-25 22:36:36 -0300

    create entity subscription

---
+++ src/main/java/com/wendelNeres/EventsMicroservices/domain/entities/Subscription.java
@@ -0,0 +1,65 @@
+package com.wendelNeres.EventsMicroservices.domain.entities;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "subscription")
+public class Subscription {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Event event;
+    private String participantEmail;
+
+
+    public Subscription(){
+
+    }
+
+    public Subscription(Long id, Event event, String participantEmail){
+        this.id = id;
+        this.event = event;
+        this.participantEmail = participantEmail;
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Event getEvent() {
+        return event;
+    }
+
+    public void setEvent(Event event) {
+        this.event = event;
+    }
+
+    public String getParticipantEmail() {
+        return participantEmail;
+    }
+
+    public void setParticipantEmail(String participantEmail) {
+        this.participantEmail = participantEmail;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Subscription that)) return false;
+        return Objects.equals(getEvent(), that.getEvent()) && Objects.equals(getParticipantEmail(), that.getParticipantEmail());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getEvent(), getParticipantEmail());
+    }
+}
