54d35be9fd93e21234ba79aaf145192253101802
WendelNeres <wendelneres.dev@gmail.com>
2025-05-25 22:01:23 -0300

    create entity event

---
+++ src/main/java/com/wendelNeres/EventsMicroservices/domain/entities/Event.java
@@ -0,0 +1,79 @@
+package com.wendelNeres.EventsMicroservices.domain.entities;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+
+
+@Entity
+@Table(name = "event")
+public class Event {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    private int maxParticipants;
+    private String data;
+    private String title;
+    private String description;
+
+
+    public Event (){
+
+    }
+
+    public Event(String id, String data, String title, String description){
+        this.id = id;
+        this.data = data;
+        this.title = title;
+        this.description = description;
+
+    }
+
+
+    public String getId (){
+        return this.id;
+    }
+
+    public void setId(String id){
+        this.id = id;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Event event)) return false;
+        return  Objects.equals(getData(), event.getData()) && Objects.equals(getTitle(), event.getTitle()) && Objects.equals(getDescription(), event.getDescription());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getData(), getTitle(), getDescription());
+    }
+}
