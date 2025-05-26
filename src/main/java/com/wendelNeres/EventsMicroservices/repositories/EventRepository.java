320ea92744c05e918e64ddc9bbb2aa0e71f69d07
WendelNeres <wendelneres.dev@gmail.com>
2025-05-26 20:10:24 -0300

    custom query

--- src/main/java/com/wendelNeres/EventsMicroservices/repositories/EventRepository.java
+++ src/main/java/com/wendelNeres/EventsMicroservices/repositories/EventRepository.java
@@ -1,10 +1,30 @@
 package com.wendelNeres.EventsMicroservices.repositories;
 
 import com.wendelNeres.EventsMicroservices.domain.entities.Event;
+import jakarta.annotation.Nonnull;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 
 
 @Repository
 public interface EventRepository extends JpaRepository<Event, String> {
+
+    @Query(value = "SELECT * FROM events e WHERE PARSE(e.date AS TIMESTAMP) > :currentDate",nativeQuery = true)
+    List<Event> findUpcomingEvents(@Param("currentDate")LocalDateTime currentDate);
+
+
+
+
+    @Nonnull
+    Optional <Event> findById (@Nonnull String id);
+
+
+
+
 }
