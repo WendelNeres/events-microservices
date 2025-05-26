f087ec712ff90359c68b8ecd671e71ebb4730863
WendelNeres <wendelneres.dev@gmail.com>
2025-05-25 21:44:37 -0300

    create project

---
+++ src/main/java/com/wendelNeres/EventsMicroservices/EventsMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.wendelNeres.EventsMicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventsMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventsMicroservicesApplication.class, args);
+	}
+
+}
